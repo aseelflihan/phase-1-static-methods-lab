bb6a7d29afaca9bf2c603084756c44a3b1e24feb
aseelflihan <aseelflihan@gmail.com>
2023-03-18 16:22:13 +0300

    complate assignment

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  static sanitize(str){
+    return str.replaceAll(/[^A-Za-z0-9 \' \-]/g, '');
+  }
+  static titleize(str){
+    const small_words = ['a', 'an', 'the', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return str.split(' ').map((word, index) => {
+      if(index !== 0 && small_words.includes(word))
+        return word.toLowerCase();
+      else
+        return this.capitalize(word);
+    }).join(' ');
+  }
 }
